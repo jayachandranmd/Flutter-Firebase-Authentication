bd24c6fde174cbcf5de55902b130971eaf37aed0
jayachandranmd <jayachandranmd.contact@gmail.com>
2022-08-09 21:49:06 +0530

    Firebase authentication demo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_authentication_demo/screens/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
